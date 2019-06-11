04a85eea1d100d5e9dbe6e801d9f0b0d426b9da0
wojtekkazek <wojciech.kazek@gmail.com>
2019-06-11 21:36:04 +0100

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,6 +5,15 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
         Calculator calculator = new Calculator();
 
         int result1 = calculator.add(2,3);
